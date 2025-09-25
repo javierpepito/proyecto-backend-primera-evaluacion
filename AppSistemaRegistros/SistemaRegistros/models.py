6b1ace80556e4036e6bee1df4658fae8d529dcf9
javierpepito <javiermendezsepulveda19@gmail.com>
2025-09-25 20:56:52 -0300

    feat: Cambios en models.py
    pequeños cambios en distintas partes.

--- AppSistemaRegistros/SistemaRegistros/models.py
+++ AppSistemaRegistros/SistemaRegistros/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+#Modelo del visitante con campo fecha y hora por separado para facilitar su filtrado.
 class Visitante(models.Model):
+    rut = models.CharField(max_length=10)
     nombre = models.CharField(max_length=100)
+    motivo_visita = models.TextField(max_length=500)
+    fecha_visita = models.DateField()
+    hora_visita = models.TimeField()
+
+    def __str__(self):
+        return self.nombre
+    
