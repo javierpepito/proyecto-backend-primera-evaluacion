71a76439350c59921d0bd8dbae601a804613d51c
javierpepito <javiermendezsepulveda19@gmail.com>
2025-10-02 14:59:57 -0300

    refactor: Se arregla validacion de hora
    :)

--- AppSistemaRegistros/SistemaRegistros/forms.py
+++ AppSistemaRegistros/SistemaRegistros/forms.py
@@ -3,8 +3,8 @@ from .models import Visita
 from django.core.exceptions import ValidationError
 from .utils import validar_rut, formatear_rut
 import re
-from datetime import date
-
+from datetime import date, timedelta, datetime
+ 
 class VisitaForm(forms.ModelForm):
     class Meta:
         model = Visita
@@ -59,10 +59,31 @@ class VisitaForm(forms.ModelForm):
 
         return fecha_visita
 
-    def clean_hora_salida(self):
+    """def clean_hora_salida(self):
         hora_salida = self.cleaned_data.get('hora_salida')
         hora_entrada = self.cleaned_data.get('hora_entrada')
         if hora_salida and hora_entrada:
             if hora_salida < hora_entrada:
                 raise ValidationError("La hora de salida no puede ser anterior a la hora de entrada.")
+        return hora_salida"""
+
+    def clean_hora_salida(self):
+        hora_salida = self.cleaned_data.get('hora_salida')
+        hora_entrada = self.cleaned_data.get('hora_entrada')
+
+        if hora_salida and hora_entrada:
+            today = datetime.today().date()
+            dt_entrada = datetime.combine(today, hora_entrada)
+            dt_salida = datetime.combine(today, hora_salida)
+
+            # Permitimos que la hora sea pasado la media noche interpretando que es otro dia.
+            if dt_salida < dt_entrada:
+                dt_salida += timedelta(days=1)
+
+            duracion = dt_salida - dt_entrada
+
+            # Que la visita no dure mas de 12 horas.
+            if duracion.total_seconds() > 12 * 3600:
+                raise ValidationError("La hora de salida no puede ser antes que la hora de entrada, ni tampoco durar más de 12 horas.")
+
         return hora_salida
