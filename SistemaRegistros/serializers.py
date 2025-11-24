9b88e5dfb8893672642cb04dedf6f6c95c457e34
javierpepito <javiermendezsepulveda19@gmail.com>
2025-11-24 09:50:47 -0300

    Serializador

--- SistemaRegistros/serializers.py
+++ SistemaRegistros/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Group, User
 from rest_framework import serializers
+from models import *
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,8 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ["url", "name"]
+
+class VisitaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Visita
+        fields = ["url", "rut", "nombre", "motivo_visita", "fecha_visita", "hora_entrada", "hora_salida"]
