2a521bf4bfefd2ad9b62241b8107dff1cb08f360
javierpepito <javiermendezsepulveda19@gmail.com>
2025-11-25 23:21:23 -0300

    Permitir de todos lados

--- AppSistemaRegistros/settings.py
+++ AppSistemaRegistros/settings.py
@@ -36,17 +36,17 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 #Desde donde se permite el uso de la api
-CORS_ALLOWED_ORIGINS = [
+"""CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5500",
     "http://localhost:4200",
     "http://127.0.0.1:8000",
     "http://localhost:64451",
     # Dominio desde donde se ocupe la API
-]
+]"""
 
 #Permite la enviar solicitudes a la api desde todos lados
-#CORS_ALLOWED_ALL_ORIGINS = True
+CORS_ALLOWED_ALL_ORIGINS = True
 
 # Application definition
 
